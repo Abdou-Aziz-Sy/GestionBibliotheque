d055fd6d0e76d3b3542cdda6b951fe3eb118312a
Mamadou Sy <mamadoujuniorsy@gmail.com>
2024-04-14 12:57:31 +0000

    Ajout gestion utilisateur

--- Bibliotheque.java
+++ Bibliotheque.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Bibliotheque {
     private ArrayList<Livre> listeLivres;
     private HashMap<Utilisateur, ArrayList<Livre>> empruntsUtilisateur;
-
+    private ArrayList<Utilisateur> utilisateurs;
     public Bibliotheque() {
         this.listeLivres = new ArrayList<>();
         this.empruntsUtilisateur = new HashMap<>();
@@ -104,6 +104,50 @@ public class Bibliotheque {
             livresEmpruntesUtilisateur.remove(livre);
             empruntsUtilisateur.put(utilisateur, livresEmpruntesUtilisateur);
         }
+    }
+    // Méthode pour ajouter un nouvel utilisateur
+    public void ajouterUtilisateur() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez saisir les informations de l'utilisateur :");
+        System.out.print("Nom : ");
+        String nom = scanner.nextLine();
+        String status = null;
+        //Conrole de saisie pour empécher l'utilisateur de saisir n'importe quoi
+        do{
+            System.out.println("Quel est le statut de l'utilisateur(utilisateur ou bibliothecaire)"); 
+             status = scanner.nextLine();  
+        }while(status!="utilisateur" || status!="bibliothecaire");
+        
+        System.out.print("Numéro d'identification : ");
+        int numeroIdentification = scanner.nextInt();
+        scanner.nextLine(); // Pour consommer le retour à la ligne après nextInt()
+        
+        Utilisateur utilisateur = new Utilisateur(nom, numeroIdentification,status);
+        utilisateurs.add(utilisateur);
+        scanner.close();
+    }
+
+    // Méthode pour supprimer un utilisateur
+    public void supprimerUtilisateur() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Entrez le numéro d'identification de l'utilisateur à supprimer : ");
+        int numeroIdentification = scanner.nextInt();
+        
+        Utilisateur utilisateurASupprimer = null;
+        for (Utilisateur utilisateur : utilisateurs) {
+            if (utilisateur.getNumeroIdentification() == numeroIdentification) {
+                utilisateurASupprimer = utilisateur;
+                break;
+            }
+        }
+        
+        if (utilisateurASupprimer != null) {
+            utilisateurs.remove(utilisateurASupprimer);
+            System.out.println("L'utilisateur a été supprimé avec succès.");
+        } else {
+            System.out.println("Utilisateur non trouvé.");
+        }
+        scanner.close();
     }
         
     public void afficherStatistiquesBibliothèque() {
